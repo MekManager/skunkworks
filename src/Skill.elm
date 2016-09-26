6ef323a45824d5a0f78e4049f0bec212e0a197f3
Eric Hopkins <eric.on.tech@gmail.com>
2016-09-26 13:57:20 -0400

    Return -1 for skills below the xp threshold

--- src/Skill.elm
+++ src/Skill.elm
@@ -6,7 +6,6 @@ import Maybe exposing (withDefault)
 import XpTable exposing (standard, fastLearner, slowLearner)
 
 
---, tiered : List List String
 type alias Skill =
   { name : String
   , subSkills : List String
@@ -24,11 +23,25 @@ type alias Skill =
 value : Skill -> String -> Int
 value skill learning =
   case learning of
-    "standard"    -> getSkillIndex standard skill.xp
-    "fastLearner" -> getSkillIndex fastLearner skill.xp
-    "slowLearner" -> getSkillIndex slowLearner skill.xp
+    "standard"    -> skillValue standard skill.xp
+    "fastLearner" -> skillValue fastLearner skill.xp
+    "slowLearner" -> skillValue slowLearner skill.xp
     _             -> 0
 
-getSkillIndex : List (Int, Int) -> Int -> Int
-getSkillIndex vals xp =
-  fst (withDefault (0, 0) (head (filter (\x -> xp >= snd x) (reverse vals))))
+{-| Get the value of a skill based off learning and xp of a skill
+  Implementation details:
+  Reading this from the inside out, the first action is to reverse the learning
+  values. This is done so that the highest value to be greater than or equal to
+  the skills xp is first, instead of last. Next we apply the greater than or
+  equal to filter on the learning set. `snd` is used because a learning set is
+  a tuple of two values like `(index, value)`. This will give a list of of only
+  the tuples where the value is gte the xp of the skill. Now take the head of
+  that list. Because a list may be empty, head returns a `Maybe`. The index of
+  the list is always the skills value, so we use `fst` to get the first part of
+  the tuple. However, `fst` does not know how to handle a `Maybe`. Thus a
+  default must be provided, if `head` returns `Nothing`, then the default tuple
+  will be provided to `fst`.
+-}
+skillValue : List (Int, Int) -> Int -> Int
+skillValue vals xp =
+  fst (withDefault (-1, -1) (head (filter (\x -> xp >= snd x) (reverse vals))))
--- tests/SkillTest.elm
+++ tests/SkillTest.elm
@@ -24,7 +24,13 @@ basics =
       }
   in
     describe "A Skill"
-      [ test "should have a value" <|
+      [ test "should have a value of 1 with 40XP and standard learning" <|
           \_ ->
             (value skill "standard") `equal` 1
+      , test "should have a 0 with 20XP and standard learning" <|
+          \_ ->
+            (value { skill | xp = 20 } "standard") `equal` 0
+      , test "should have a -1 with less than 20XP and standard learning" <|
+          \_ ->
+            (value { skill | xp = 10 } "standard") `equal` -1
       ]
