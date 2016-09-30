1cf6a6372bf328e315970bb1cffc0012f88a9b6a
Eric Hopkins <eric.on.tech@gmail.com>
2016-09-30 02:14:44 -0400

    Create a basic character layout with debug area

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,68 @@
-import Html exposing (div, button, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Html.App exposing (beginnerProgram)
-import Html.Events exposing (onClick)
+
+import Character exposing (Character, characterFactory)
 
 
 main =
-  beginnerProgram { model = 0, view = view, update = update }
+  beginnerProgram { model = characterFactory, view = view, update = update }
 
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    [ h1 [] [ text "Character Creator" ]
+    , hr [] []
+    , div [ class "grid" ]
+        [ div [ class "cell -6of12" ]
+          [ Html.form []
+              [ fieldset [ class "form-group" ]
+                  [ label [ for "firstName" ] [ text "First Name:" ]
+                  , input [ id "firstName"
+                          , class "form-control"
+                          , type' "text"
+                          , onInput FirstName ] [ text model.firstName ]
+                  ]
+              , fieldset [ class "form-group" ]
+                  [ label [ for "lastName" ] [ text "First Name:" ]
+                  , input [ id "lastName"
+                          , class "form-control"
+                          , type' "text"
+                          , onInput LastName ] [ text model.lastName ]
+                  ]
+              , fieldset [ class "form-group form-textarea" ]
+                  [ label [ for "concept" ] [ text "Concept:" ]
+                  , textarea [ id "concept"
+                             , class "form-control"
+                             , rows 3
+                             , onInput Concept ] [ text model.concept ]
 
-type Msg = Increment | Decrement
+                  ]
+              ]
+          ]
+        , div [ class "cell -6of12" ]
+          [
+            text "wow"
+          ]
+        ]
+    , div []
+      [ hr [] []
+      , p [] [ text <| "first name: " ++ model.firstName ]
+      , p [] [ text <| "last name: " ++ model.lastName ]
+      , p [] [ text <| "concept: " ++ model.concept ]
+      ]
+    ]
+    -- , button [ class "btn btn-default", onClick Decrement ] [ text "cool" ]
+    -- , div [] [ text model.firstName ]
+    -- , button [ class "btn btn-default", onClick Increment ] [ text "woah" ]
+
+type Msg
+  = FirstName String
+  | LastName String
+  | Concept String
 
 update msg model =
   case msg of
-    Increment -> model + 1
-    Decrement -> model - 1
+    FirstName name -> { model | firstName = name }
+    LastName name  -> { model | lastName  = name }
+    Concept text   -> { model | concept = text }
