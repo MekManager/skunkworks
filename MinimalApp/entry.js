4a38a62a55dbe2507220c8267191fefa9f8469a5
Eric Hopkins <eric.on.tech@gmail.com>
2016-10-09 17:02:48 -0400

    Introduce redux and more handlebar tempates

--- MinimalApp/entry.js
+++ MinimalApp/entry.js
@@ -1,9 +1,36 @@
 import $ from 'jquery'
+import { createStore } from 'redux'
+
+const renderState = (state) => {
+  $('#amount').text(state.xp)
+  $('#debugFName').text(state.firstName)
+  $('#debugLName').text(state.lastName)
+}
+
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'FIRST_NAME':
+      return Object.assign({}, state, { firstName: action.value })
+    case 'LAST_NAME':
+      return Object.assign({}, state, { lastName: action.value })
+    default:
+      return state
+  }
+}
+
+let store = createStore(reducer)
+store.subscribe(() => renderState(store.getState()))
 
 $(document).ready(function () {
-  const name = 'Eric'
+  $('#characterFluff').append(require('./textFormControl.hbs')({id: 'firstName', label: 'First Name:'}),
+                              require('./textFormControl.hbs')({id: 'lastName', label: 'Last Name:'}),
+                              require('./textAreaFormControl.hbs')({id: 'concept', label: 'Concept:'}))
+  renderState({ firstName: '', lastName: '', xp: 5000 })
 
-  $('#app').append(require('./test.hbs')({title: 'Wow! Handlebars', text: 'whatever'}))
-  $('#inject').text(`hi there! ${name}`)
-  $('#pow').text(`2 to the power of 2 is ${2 ** 2}`)
+  $('#firstName').on('keyup', () => {
+    store.dispatch({ type: 'FIRST_NAME', value: $('#firstName').val() })
+  })
+  $('#lastName').on('keyup', () => {
+    store.dispatch({ type: 'LAST_NAME', value: $('#lastName').val() })
+  })
 })
